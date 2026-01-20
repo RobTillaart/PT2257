d5f112b2eef77460a72b138df8e4961102de727e
Rob Tillaart <rob.tillaart@gmail.com>
2026-01-20 22:16:43 +0100

    initial version

--- examples/PT2257_demo/PT2257_demo.ino
+++ examples/PT2257_demo/PT2257_demo.ino
@@ -1,7 +1,7 @@
 //
 //    FILE: PT2257_demo.ino
 //  AUTHOR: Rob Tillaart
-// PURPOSE: test basic behaviour and performance
+// PURPOSE: test basic behaviour
 //     URL: https://github.com/RobTillaart/PT2257
 
 
@@ -24,12 +24,45 @@ void setup()
   myPT.begin();
 
   myPT.allOff();
+
+  myPT.mute(true);
+  Serial.println(myPT.isMuted());  //  expect 1
+  myPT.mute(false);
+  Serial.println(myPT.isMuted());  //  expect 0
 }
 
 
 void loop()
 {
+  for (int db = -79; db <= -45; db++)
+  {
+    myPT.stereo(db);
+    Serial.print(myPT.getLeft());
+    Serial.print(" - ");
+    Serial.println(myPT.getRight());
+    delay(100);
+  }
+  Serial.println();
 
+  for (int db = -45; db >= -79; db--)
+  {
+    myPT.right(db);
+    Serial.print(myPT.getLeft());
+    Serial.print(" - ");
+    Serial.println(myPT.getRight());
+    delay(100);
+  }
+  Serial.println();
+
+  for (int db = -45; db >= -79; db--)
+  {
+    myPT.left(db);
+    Serial.print(myPT.getLeft());
+    Serial.print(" - ");
+    Serial.println(myPT.getRight());
+    delay(100);
+  }
+  Serial.println("\n\n\n");
   delay(2000);
 }
 
